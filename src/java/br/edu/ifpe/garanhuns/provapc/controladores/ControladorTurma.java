62ef1cf73b339c146d7a0660531045da0938f1c7
Maria Thais de Melo Costa <thaiscapoeiras1999@hotmail.com>
2017-08-25 20:54:31 -0300

    adicionando controlador turma

---
+++ src/java/br/edu/ifpe/garanhuns/provapc/controladores/ControladorTurma.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.provapc.controladores;
+
+import br.edu.ifpe.garanhuns.provapc.model.Turma;
+import br.edu.ifpe.garanhuns.provapc.model.dao.FabricaRepositorios;
+import br.edu.ifpe.garanhuns.provapc.model.dao.RepositorioGenerico;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Thais
+ */
+@ManagedBean
+@SessionScoped
+public class ControladorTurma {
+    private Turma selected =null;
+    private Turma alterando=null;
+    
+    RepositorioGenerico<Turma, Integer> repositorio = null;
+    
+    public ControladorTurma(){
+        this.repositorio= FabricaRepositorios.fabricarRepositorio(FabricaRepositorios.turma, FabricaRepositorios.BD);
+    }
+    public String adicionar(Turma t){
+        repositorio.inserir(t);
+        return "ApresentarTurma.xhtml";
+    }
+    public void remover(Turma t){
+        repositorio.excluir(t);
+    }
+    
+    public void alterar(Turma t){
+        repositorio.alterar(t);
+    }
+    
+    public void recuperar(int id){
+        repositorio.recuperar(id);
+    }
+    
+    public List<Turma> recuperarTodos(){
+       return  repositorio.recuperarTodos();
+    }
+
+    public Turma getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Turma selected) {
+        this.selected = selected;
+    }
+
+    public Turma getAlterando() {
+        return alterando;
+    }
+
+    public void setAlterando(Turma alterando) {
+        this.alterando = alterando;
+    }
+    
+    public void remover(){
+        remover(selected);
+    }
+    
+    public String alterar(){
+        this.alterando=selected;
+        return "CadastrarTurma.xhtml";
+    }
+    
+}
