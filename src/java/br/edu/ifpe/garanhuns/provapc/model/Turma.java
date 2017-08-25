c7b7e3e3ef1b80c81d11e1bd1c04ba6211d99b10
Maria Thais de Melo Costa <thaiscapoeiras1999@hotmail.com>
2017-08-25 20:19:48 -0300

    criando entidade turma

---
+++ src/java/br/edu/ifpe/garanhuns/provapc/model/Turma.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.provapc.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Thais
+ */
+@Entity
+@Table
+public class Turma {
+    
+    @Id
+    @GeneratedValue
+    long id;
+    @Column
+    private String turma;
+    
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public Turma(String turma) {
+        this.turma = turma;
+    }
+    
+    
+    public Turma() {
+    }
+    
+    
+    
+    
+}
