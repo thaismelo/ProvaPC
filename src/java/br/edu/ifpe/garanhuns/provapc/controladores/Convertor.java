1e7e84b9e1d11fd8659ba70df40687fc2a0822e5
Maria Thais de Melo Costa <thaiscapoeiras1999@hotmail.com>
2017-09-02 18:36:39 -0300

    fazendo conversor para o select de turma

---
+++ src/java/br/edu/ifpe/garanhuns/provapc/controladores/Convertor.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.provapc.controladores;
+
+import br.edu.ifpe.garanhuns.provapc.model.Turma;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+import javax.faces.convert.Converter;
+
+/**
+ *
+ * @author Thais
+ */
+@FacesConverter("convertor")
+public class Convertor implements Converter{
+
+    
+    public Turma getAsObject(FacesContext fc, UIComponent uic, String value) {
+        if(value != null && value.trim().length() > 0) {
+            try {
+               ControladorTurma service = (ControladorTurma) fc.getExternalContext().getApplicationMap().get("ControladorTurma");
+                return service.recuperar(Integer.parseInt(value));
+            } catch(NumberFormatException e) {
+                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Not a valid theme."));
+            }
+        }
+        else {
+            return null;
+        }
+    }
+ 
+    
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic,Object turma) {
+        if(turma != null) {
+            return String.valueOf(((Turma) turma).getId());
+        }
+        else {
+            return null;
+        }
+    }   
+    
+   
+    
+}
