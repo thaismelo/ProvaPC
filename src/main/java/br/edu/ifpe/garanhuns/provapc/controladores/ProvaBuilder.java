f8bb9bd5b5ee471d4e346f619d1be0dd822f211c
Gabriel Alves <gabrielalves3d@gmail.com>
2018-01-25 00:58:19 -0300

    Adaptando ProvaBuilder a nova classe prova

--- src/main/java/br/edu/ifpe/garanhuns/provapc/controladores/ProvaBuilder.java
+++ src/main/java/br/edu/ifpe/garanhuns/provapc/controladores/ProvaBuilder.java
@@ -10,9 +10,13 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
+import br.edu.ifpe.garanhuns.provapc.model.Professor;
 import br.edu.ifpe.garanhuns.provapc.model.Prova;
 import br.edu.ifpe.garanhuns.provapc.model.Questao;
+import br.edu.ifpe.garanhuns.provapc.model.RespostaProva;
+import br.edu.ifpe.garanhuns.provapc.model.Turma;
 
 /**
  *
@@ -22,10 +26,32 @@ import br.edu.ifpe.garanhuns.provapc.model.Questao;
 @RequestScoped
 public class ProvaBuilder {
     
-    private long id;
+    public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+
+
+	private long id;
     private String titulo;
+    private Turma turma;
+    private Professor professor = (Professor) FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+				.get("professorLogado");;
     private boolean alterando = false;
     List<QuestaoBuilder> questoes = new ArrayList<QuestaoBuilder>();
+    List<RespostaProva> notas = new ArrayList<RespostaProva>();
+    
 
     public ProvaBuilder() {
         
@@ -34,6 +60,8 @@ public class ProvaBuilder {
     public ProvaBuilder(Prova selected) {
         this.id = selected.getId();
         this.titulo = selected.getTitulo();
+        this.professor=selected.getProfessor();
+        this.turma=selected.getTurma();
         this.alterando = true;
     }
     
@@ -76,9 +104,19 @@ public class ProvaBuilder {
         return questoes.remove(o);
     }
     
-    
+    public List<RespostaProva> getNotas() {
+        return notas;
+    }
+
+    public boolean addNota(RespostaProva r) {
+        return notas.add(r);
+    }
+
+    public boolean removeNota(RespostaProva r) {
+        return notas.remove(r);
+    }
     public Prova construir(){
-        Prova novaProva = new Prova(id,titulo);
+        Prova novaProva = new Prova(id,titulo,professor,turma,notas);
         for(QuestaoBuilder q : this.questoes){
             Questao qu = q.construir();
             qu.setProva(novaProva);
